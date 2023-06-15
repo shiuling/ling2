448239fbdcaf99c0056624c081089e30d25cc706
shiuling <134581306+shiuling@users.noreply.github.com>
2023-06-15 15:44:51 +0800

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -7,13 +7,13 @@ export default function Success() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox Template!</title>
+        <title>Next.js 歡迎來到 工具箱首頁!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1>Submission Received!</h1>
-        <p>Thank you for your feedback! Head back to the <Link href="/"><a>home page</a></Link>.</p>
+        <p>感謝你的臉書提供有關工具箱相關的訊息!  <Link href="/"><a>home page</a></Link>.</p>
       </main>
 
       <Footer />
